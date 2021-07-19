4f6d72289c3ab6f5f4393275cd96b9a8b0134a15
martin-moringa <martin.mylles@student.moringachool.com>
2021-07-19 23:25:31 +0300

    Create edit profile template and allow users to edit their profile

--- myawwards/urls.py
+++ myawwards/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path, include
 from . import views
 from rest_framework import routers
-
 router = routers.DefaultRouter()
 router.register('profile', views.ProfileViewSet)
 router.register('users', views.UserViewSet)
-
 urlpatterns = [
     path('', views.index, name='index'),
     path('signup/', views.signup, name='signup'),
@@ -13,4 +11,6 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('profile/<username>/', views.profile, name='profile'),
+    path('profile/<username>/settings', views.edit_profile, name='edit'),
+    path('project/<post>', views.project, name='project')
 ]
